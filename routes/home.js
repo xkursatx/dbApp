9a6f87d534fec65bf6584d84f8309560c1397d07
Kürşat Çakır <kursat.cakir@outlook.com>
2022-02-28 23:28:06 +0300

    added mongodb database list

--- routes/home.js
+++ routes/home.js
@@ -1,18 +1,4 @@
 const express = require("express");
 const router = express.Router();
 
-const MongoClient = require("mongodb").MongoClient;
-
-router.get("/", (req, res) => {
-  MongoClient.connect("mongodb://127.0.0.1/dbAppDB", (err, db) => {
-    if (err) throw err;
-    const adminDb = db.db("admin").admin();
-
-    adminDb
-      .listDatabases()
-      .then(({ databases }) => res.send(databases))
-      .catch((err) => console.log(err));
-  });
-});
-
 module.exports = router;
