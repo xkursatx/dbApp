e550db2d7013d67b5107723f15dce289bff9bf95
Kürşat Çakır <kursat.cakir@outlook.com>
2022-02-28 23:27:42 +0300

    start up

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ const mongoRoute = require("./routes/mongo");
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/", homeRoute);
 app.use("/mongo", mongoRoute);
+
+const port = process.env.PORT || 2001;
+app.listen(port, () => {
+  console.log(`express listening on port: ${port}`);
+});
